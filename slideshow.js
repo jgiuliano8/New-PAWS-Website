1e45da1479172941dd25c1cf449a7883a1af303b
Jeff Giuliano <jgiuliano8@yahoo.com>
2021-10-28 13:12:09 -0400

    Slideshow functionality under timed interval. Need to make dots clickable.

---
+++ slideshow.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const dots = document.querySelectorAll(".dots__dot");
+const slides = document.querySelectorAll(".slideshow__img");
+const slideshowElem = document.querySelector(".slideshow");
+
+let currSlide = 1;
+const maxSlides = slides.length;
+
+function setCurrDot() {
+  dots.forEach(function (dot) {
+    if (dot.classList.contains("dots__dot--current"))
+      dot.classList.remove("dots__dot--current");
+  });
+  dots.forEach(function (dot) {
+    if (dot.dataset.dotNumber == currSlide)
+      dot.classList.add("dots__dot--current");
+  });
+}
+
+function nextSlide() {
+  slideshowElem.classList.remove(`slideshow--${currSlide}`);
+  currSlide++;
+  if (currSlide > maxSlides) {
+    currSlide = 1;
+  }
+
+  slides.forEach(function (slide) {
+    if (slide.classList.contains("slideshow__img--current"))
+      slide.classList.remove("slideshow__img--current");
+  });
+
+  slides.forEach(function (slide) {
+    if (slide.classList.contains(`slideshow__img--${currSlide}`))
+      slide.classList.add("slideshow__img--current");
+  });
+
+  slideshowElem.classList.add(`slideshow--${currSlide}`);
+
+  setCurrDot();
+}
+
+setInterval(nextSlide, 4000);
